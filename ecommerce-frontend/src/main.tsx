5c0fb3292ad3a4cb26d28c95fae7f28936107eb7
zeroual01 <zeroual01@gmail.com>
2024-08-20 16:25:48 +0100

    create app store

--- ecommerce-frontend/src/main.tsx
+++ ecommerce-frontend/src/main.tsx
@@ -1,12 +1,17 @@
 
 
 import { createRoot } from 'react-dom/client'
-
+//redux
+import { Provider } from 'react-redux'
+import store from '@redux/store'
 //styles 
 import '@styles/global.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from '@routes/App'
 
 createRoot(document.getElementById('root')!).render(
-        <App />
+        <Provider store={store}>
+                <App />
+        </Provider>
+       
 )
