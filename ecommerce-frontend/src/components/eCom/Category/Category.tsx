755c147136217d3fea501cf91e220eb3f25aa71c
zeroual01 <zeroual01@gmail.com>
2024-08-21 17:15:16 +0100

    Fix Categories Image Rendering skeleton

--- ecommerce-frontend/src/components/eCom/Category/Category.tsx
+++ ecommerce-frontend/src/components/eCom/Category/Category.tsx
@@ -1,18 +1,27 @@
 import styles from './category.module.css'
 import { Link } from 'react-router-dom';
 import { TCategory } from '@customtypes/category';
-const {category, categoryImg, categoryTitle} = styles;
+const {category, categoryImg, categoryTitle, skeleton} = styles;
+import { useState } from 'react';
 
 
 
 const Category = ({title, img, prefix} : TCategory) => {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handlImageLoad = () => {
+    setIsLoading(false);
+  }
   return (
     <div className={category}>
         <Link to={`/products/${prefix}`}>
           <div className={categoryImg}>
-            <img  
-                src={img}
-                alt={title} />
+            { isLoading && <div className={skeleton}></div>}
+            <img 
+                src={img} alt={title}
+                style={{ display: isLoading ? 'none' : 'block' }}
+                onLoad={handlImageLoad} />
           </div>
           <h4 className={categoryTitle}>{ title }</h4>
         </Link>
