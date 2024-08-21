cf5f2f39c009316e76d445244365f26bf49cc173
zeroual01 <zeroual01@gmail.com>
2024-08-21 12:39:01 +0100

    extraRudecers

--- ecommerce-frontend/src/redux/categories/categoriesSlice.ts
+++ ecommerce-frontend/src/redux/categories/categoriesSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+import actGetCategories from "./thunk/actGetCategories";
 export interface ICategoriesStatus {
     records : { id : number, title : string, prefix : string, img : string}[];
     loading : 'idle' | 'pending' | 'succeeded' | 'failed',
@@ -14,7 +14,26 @@ const initialState : ICategoriesStatus = {
 const categoriesSlice = createSlice({
     name : 'categories',
     initialState,
-    reducers :{}
+    reducers :{},
+    extraReducers : (builder) => {
+        builder
+            .addCase(actGetCategories.pending, (state) => {
+                state.loading = 'pending'
+                state.error = null
+            })
+            .addCase(actGetCategories.fulfilled, (state, action) => {
+                state.loading = 'succeeded'
+                state.records = action.payload
+            })
+            .addCase(actGetCategories.rejected, (state, action) => {
+                state.loading = 'failed'
+                if(action.error.message && typeof action.error.message === 'string') {
+                    state.error = action.error.message
+                }
+                // state.error = action.error.message as string
+            })
+    }
 })
 
+export {actGetCategories}
 export default categoriesSlice.reducer
