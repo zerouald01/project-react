659a3879a6542f4a2abe5ab0ba9827f9eb30e330
zeroual01 <zeroual01@gmail.com>
2024-08-23 16:23:05 +0100

    Save Cart Items Into LocalStorage by persist redux

--- ecommerce-frontend/src/main.tsx
+++ ecommerce-frontend/src/main.tsx
@@ -3,7 +3,8 @@
 import { createRoot } from 'react-dom/client'
 //redux
 import { Provider } from 'react-redux'
-import store from '@redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import {store, persistore} from '@redux/store'
 //styles 
 import '@styles/global.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -11,7 +12,8 @@ import App from '@routes/App'
 
 createRoot(document.getElementById('root')!).render(
         <Provider store={store}>
-                <App />
-        </Provider>
-       
+                <PersistGate loading={null} persistor={persistore}> 
+                        <App />
+                </PersistGate>     
+        </Provider>   
 )
--- ecommerce-frontend/src/redux/store.ts
+++ ecommerce-frontend/src/redux/store.ts
@@ -1,15 +1,35 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import categoriesReducer from './categories/categoriesSlice'
 import productsReducer from './products/productsSlice'
 import cartReducer from './cart/cartSlice'
+import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from 'redux-persist'
+
+
+
+
+const cartPersistConfig = {
+  key : 'cart',
+  storage,
+}
+
+const persistedCartReducer = persistReducer(cartPersistConfig, cartReducer);
+
+const rootRudcer = combineReducers({
+  categoriesReducer,
+  productsReducer,
+  cartReducer : persistedCartReducer
+})
 
 export const store = configureStore({
-  reducer: { categoriesReducer, productsReducer, cartReducer},
+  reducer: rootRudcer,
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
+
+export const persistore = persistStore(store)
 
 export default  store
