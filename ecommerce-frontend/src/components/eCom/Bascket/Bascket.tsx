51b03ea281bb9100537b0cc4f7d2b3c8399eaff8
zeroual01 <zeroual01@gmail.com>
2024-08-23 18:10:39 +0100

    animate shop cart & debouncing add to cart action

--- ecommerce-frontend/src/components/eCom/Bascket/Bascket.tsx
+++ ecommerce-frontend/src/components/eCom/Bascket/Bascket.tsx
@@ -2,17 +2,26 @@ import Logo from '@assets/cart.svg?react';
 import styles from './bascket.module.css'
 import { useAppSelector } from '@redux/hooks';
 import { selectTotalQuantity } from '@redux/cart/cartSlice';
-// import { getCartTotalQuantity } from '@redux/cart/cartSlice';
+import { useState, useEffect } from 'react';
 
-const { basketContainer, bascketSize, basketQuantity } = styles;
+const { basketContainer, bascketSize, basketQuantity, animate } = styles;
 
 const Bascket = () => {
   const totalQuantity = useAppSelector((state) => selectTotalQuantity(state));
+  const [animateQuantity, setAnimateQuantity] = useState(false);
 
+  useEffect(() => { 
+    if (totalQuantity > 0) {
+      setAnimateQuantity(true);
+      setTimeout(() => {
+        setAnimateQuantity(false);
+      }, 3000);
+    }
+  }, [totalQuantity]);
   return (
     <div className={basketContainer}>
       <Logo aria-label="basket icon" className={bascketSize} />
-      <div className={basketQuantity}>{totalQuantity}</div>
+      <div className={`${basketQuantity} ${animateQuantity ? animate : ''}`}>{totalQuantity}</div>
     </div>
   );
 };
--- ecommerce-frontend/src/components/eCom/Product/Product.tsx
+++ ecommerce-frontend/src/components/eCom/Product/Product.tsx
@@ -3,15 +3,25 @@ import styles from './prod.module.css'
 import { TProduct } from "@customtypes/product"
 import { useAppDispatch } from "@redux/hooks"
 import { addToCart } from "@redux/cart/cartSlice"
+import { useState, useCallback } from "react"
 
 const {product, productImg, productTitle, productPrice, addButton } = styles
 
 const Product = ({id, title, price, img }: TProduct) => {
 
   const dispatch = useAppDispatch();
-  const addToCartHandler = () => {
-    dispatch(addToCart(id))
-  }
+  const [isAdding, setIsAdding] = useState(false);
+
+  const debounceAddToCart = useCallback(()=>{
+    if(!isAdding) {
+      setIsAdding(true);  
+      dispatch(addToCart(id));
+      setTimeout(() => {
+        setIsAdding(false);
+      }, 300);
+    }
+  },[isAdding, dispatch, id])
+ 
 
   return (
     <div className={product}>
@@ -23,7 +33,7 @@ const Product = ({id, title, price, img }: TProduct) => {
       </div>
       <h2 className={productTitle}>{title}</h2>
       <h3 className={productPrice}>{price} MAD</h3>
-      <Button variant="danger" className={addButton} onClick={addToCartHandler}>
+      <Button variant="danger" className={addButton} onClick={debounceAddToCart}>
         Add to cart
       </Button>
     </div>
